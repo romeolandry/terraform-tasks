ddea01c4420103db3c307fb7ff68c3caf7c40e60
Romeo Landry Kamgo Chetchom <kamgoche@yahoo.com>
2026-02-05 12:30:03 +0100

    create outputs.tf file

--- basics/aws-s3-bucket/main.tf
+++ basics/aws-s3-bucket/main.tf
@@ -18,7 +18,3 @@ resource "aws_s3_bucket" "terraform_name_s3_bucket" {
       enabled = true
     }
 }
-
-output "my_s3_bucket_complete_details"{
-    value = aws_s3_bucket.terraform_name_s3_bucket.versioning
-}
---
+++ basics/aws-s3-bucket/outputs.tf
@@ -0,0 +1,3 @@
+output "my_s3_bucket_complete_details"{
+    value = aws_s3_bucket.terraform_name_s3_bucket.versioning
+}
