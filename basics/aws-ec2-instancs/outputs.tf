03d470d3bd3f1930cd46c2e0e938b42a5e69efcd
Romeo Landry Kamgo Chetchom <kamgoche@yahoo.com>
2026-02-05 15:59:55 +0100

    clear output

--- basics/aws-ec2-instancs/outputs.tf
+++ basics/aws-ec2-instancs/outputs.tf
@@ -1,10 +1,10 @@
-output "aws_security_group_complete_details" {
-  value = aws_security_group.http_server_sg
-}
+# output "aws_security_group_complete_details" {
+#   value = aws_security_group.http_server_sg
+# }
 
-output "aws_instance_http_server_complete_details" {
-  value = aws_instance.http_server
-}
+# output "aws_instance_http_server_complete_details" {
+#   value = aws_instance.http_server
+# }
 
 output "aws_instance_http_server_complete_details_public_dns" {
   value = aws_instance.http_server.public_dns
