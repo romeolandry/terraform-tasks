7f508981b9d5b29a6c9a1453eb817d35c9108deb
Romeo Landry Kamgo Chetchom <kamgoche@yahoo.com>
2026-02-05 15:19:45 +0100

    add output

--- basics/aws-ec2-instancs/outputs.tf
+++ basics/aws-ec2-instancs/outputs.tf
@@ -1,3 +1,7 @@
 output "aws_security_group_complete_details" {
   value = aws_security_group.http_server_sg
 }
+
+output "aws_instance_http_server_complete_details" {
+  value = aws_instance.http_server
+}
