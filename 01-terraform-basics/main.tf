f70c890431b2cf652bf1d74d7b1e1db6ea263583
Romeo Landry Kamgo Chetchom <kamgoche@yahoo.com>
2026-02-05 11:52:16 +0100

    clear repository

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -14,4 +14,11 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_name_s3_bucket" {
     bucket = "my-s3-bucket-tf-udemy"
+    versioning {
+      enabled = true
+    }
+}
+
+output "my_s3_bucket_complete_details"{
+    value = aws_s3_bucket.terraform_name_s3_bucket.versioning
 }
